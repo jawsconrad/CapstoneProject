3ff4d54c774d239bf8df08a050bdd08702e4eb4a
nman247 <nasirdamonmartin@gmail.com>
2021-07-14 00:10:11 -0400

    7/14/2021-Prototype

--- README.md
+++ README.md
@@ -2,6 +2,6 @@
 Team 4 - Senior Capstone - MGA
 This is the first change to the CapstoneProject
 
-Edited 6/15
+Edited 7/13 by Nasir Martin
 
 Johnathan Conrad
--- index.js
+++ index.js
@@ -8,8 +8,50 @@ const fs = require('fs')
 const app = express()
 app.set("view engine", "ejs")
 
+<<<<<<< Updated upstream
 app.use("/", (req, res) => {
     res.render('index')
+=======
+app.use(express.static(__dirname + '/public'))
+app.get("/Logo.png", (req, res) => {
+    res.sendFile(path.join(__dirname, "/views/images/Logo.png"));
+  });
+app.get("/COVID.jpg", (req, res) => {
+    res.sendFile(path.join(__dirname, "/views/images/COVID-19.jpg"));
+  });
+app.get("/GroupPic.png", (req, res) => {
+    res.sendFile(path.join(__dirname, "/views/images/GroupPic.png"));
+  });
+app.get("/", (req, res) => {
+    res.render('index', {
+        title: 'Home - COVID Symptom Tracker'
+    })
+})
+app.get("/login", (req, res) => {
+    res.render('login', {
+        title: 'Login - COVID Symptom Tracker'
+    })
+})
+app.get("/about", (req, res) => {
+    res.render('about', {
+        title: 'About - COVID Symptom Tracker'
+    })
+>>>>>>> Stashed changes
+})
+app.get("/register", (req, res) => {
+    res.render('register', {
+        title: 'Register - COVID Symptom Tracker'
+    })
+})
+app.get("/admin", (req, res) => {
+        res.render('admin', {
+            title: 'Admin - COVID Symptom Tracker'
+        })
+})
+app.get("/contact", (req, res) => {
+    res.render('contact', {
+        title: 'Contact - COVID Symptom Tracker'
+    })
 })
 
 https.createServer({
